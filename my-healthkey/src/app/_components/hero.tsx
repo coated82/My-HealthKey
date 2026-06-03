ac807bca2496ea4b15dec6095b8df44075f4b47f
West-2077 <mmarcelohenriquegmh@gmail.com>
2026-06-03 04:06:30 -0300

    Criei o hero ve se ficou bom

---
+++ my-healthkey/src/app/_components/hero.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export function Hero() {
+
+    return (
+        <section className="bg-red-600 text-white  relative overflow-hidden">
+
+            <div className="container mx-auto py - 12 px - 6">
+                <article className="flex flex-col lg:flex-row items-center justify-start gap-12">
+                    <div>
+                        <h1>Foto do remedio</h1>
+                        <Image
+                            src="/Paracetamol.png"
+                            alt="Paracetamol"
+                            width={242}
+                            height={242}
+                        />
+
+                    </div>
+                    <div className="max-w-3xl">
+                        <p className="text-3xl font-normal leading-10">
+                            Procure os remedios mais baratos perto de vocẽ,sempre presando pela sua saude e seu tempo.
+                        </p>
+
+
+                    </div>
+                </article>
+            </div>
+
+
+        </section>
+
+    )
+}
--- my-healthkey/src/app/page.tsx
+++ my-healthkey/src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Header } from "./_components/header"
+import {Hero} from "./_components/hero"
 
 export default function Home(){
   return(
     <main>
       <Header />
+      <Hero />
+      
     </main>
   )
 }
